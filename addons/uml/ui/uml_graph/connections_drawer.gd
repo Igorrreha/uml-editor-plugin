5e2f3ed04b38c9b75c4b85a5552c9ba292612db2
igorecha.9999 <igorecha.9999@gmail.com>
2023-05-19 20:51:19 +0700

    Add relationships arrows types

--- addons/uml/ui/uml_graph/connections_drawer.gd
+++ addons/uml/ui/uml_graph/connections_drawer.gd
@@ -8,7 +8,7 @@ extends Node
 @export var _line_color: Color
 
 @export_group("Heading")
-@export_range(0, 360, 1) var _heading_angle: float
+@export_range(0, 360, 1) var _heading_angle_degrees: float
 @export var _heading_length: float
 
 
@@ -27,8 +27,25 @@ func on_graph_draw() -> void:
 		point_a += _get_point_on_rect(node_a.get_rect(), delta_angle)
 		point_b -= _get_point_on_rect(node_b.get_rect(), delta_angle)
 		
-		_graph.draw_line(point_a, point_b, _line_color, _line_width)
-		_draw_arrow_heading(point_b, delta_angle)
+		match connection.type:
+			UmlGraph.ConnectionType.ASSOCIATION:
+				_graph.draw_line(point_a, point_b, _line_color, _line_width)
+			
+			UmlGraph.ConnectionType.DIRECTED_ASSOCIATION:
+				_graph.draw_line(point_a, point_b, _line_color, _line_width)
+				_draw_arrow_heading(point_b, delta_angle, true, true)
+			
+			UmlGraph.ConnectionType.COMPOSITION:
+				_graph.draw_line(point_a, point_b, _line_color, _line_width)
+				_draw_diamond_heading(point_b, delta_angle, true)
+			
+			UmlGraph.ConnectionType.INHERITANCE:
+				_graph.draw_line(point_a, point_b, _line_color, _line_width)
+				_draw_arrow_heading(point_b, delta_angle, true, false)
+			
+			UmlGraph.ConnectionType.REALIZATION:
+				_graph.draw_dashed_line(point_a, point_b, _line_color, _line_width, 10)
+				_draw_arrow_heading(point_b, delta_angle, true, false)
 
 
 func _get_point_on_rect(rect: Rect2, angle: float) -> Vector2:
@@ -51,10 +68,27 @@ func _get_point_on_rect(rect: Rect2, angle: float) -> Vector2:
 	return Vector2.ZERO
 
 
-func _draw_arrow_heading(target: Vector2, angle: float) -> void:
+func _draw_arrow_heading(target: Vector2, angle: float, closed: bool = false,
+		filled: bool = false) -> void:
 	var angle_vector = Vector2.from_angle(angle)
 	var tail_delta = angle_vector * -_heading_length
-	_graph.draw_line(target, target + tail_delta.rotated(deg_to_rad(_heading_angle)),
-			_line_color, _line_width)
-	_graph.draw_line(target, target + tail_delta.rotated(deg_to_rad(-_heading_angle)),
-			_line_color, _line_width)
+	
+	var point_a = target + tail_delta.rotated(deg_to_rad(_heading_angle_degrees))
+	_graph.draw_line(target, point_a, _line_color, _line_width)
+	
+	var point_b = target + tail_delta.rotated(deg_to_rad(-_heading_angle_degrees))
+	_graph.draw_line(target, point_b, _line_color, _line_width)
+	
+	if closed or filled:
+		_graph.draw_line(point_a, point_b, _line_color, _line_width)
+	
+	if filled:
+		_graph.draw_colored_polygon([target, point_a, point_b], _line_color)
+
+
+func _draw_diamond_heading(target: Vector2, angle: float, filled: bool = false) -> void:
+	_draw_arrow_heading(target, angle, filled, filled)
+	
+	var heading_midline_length = cos(deg_to_rad(_heading_angle_degrees)) * _heading_length
+	var diamond_midline_vector = Vector2.from_angle(angle) * heading_midline_length * 2
+	_draw_arrow_heading(target - diamond_midline_vector, angle + PI, filled, filled)
--- addons/uml/ui/uml_graph/uml_graph.gd
+++ addons/uml/ui/uml_graph/uml_graph.gd
@@ -7,6 +7,15 @@ extends GraphEdit
 var _connections: Array[Connection] = []
 
 
+enum ConnectionType {
+	ASSOCIATION,
+	DIRECTED_ASSOCIATION,
+	COMPOSITION,
+	INHERITANCE,
+	REALIZATION,
+}
+
+
 func get_connections() -> Array[Connection]:
 	return _connections
 
@@ -15,8 +24,8 @@ func _on_popup_request(position) -> void:
 	_popup_menu.popup(Rect2(position, _popup_menu.size))
 
 
-func _connect(node_a: UmlNode, node_b: UmlNode) -> void:
-	_connections.append(Connection.new(node_a, node_b))
+func _connect(node_a: UmlNode, node_b: UmlNode, connection_type: ConnectionType) -> void:
+	_connections.append(Connection.new(node_a, node_b, connection_type))
 
 
 func _input(event) -> void:
@@ -28,7 +37,16 @@ func _input(event) -> void:
 			var prev_node: UmlNode
 			for node in selected_nodes:
 				if prev_node:
-					_connect(prev_node, node)
+					if Input.is_action_pressed("1"):
+						_connect(prev_node, node, ConnectionType.ASSOCIATION)
+					elif Input.is_action_pressed("2"):
+						_connect(prev_node, node, ConnectionType.DIRECTED_ASSOCIATION)
+					elif Input.is_action_pressed("3"):
+						_connect(prev_node, node, ConnectionType.COMPOSITION)
+					elif Input.is_action_pressed("4"):
+						_connect(prev_node, node, ConnectionType.INHERITANCE)
+					elif Input.is_action_pressed("5"):
+						_connect(prev_node, node, ConnectionType.REALIZATION)
 				
 				prev_node = node
 			
@@ -38,7 +56,9 @@ func _input(event) -> void:
 class Connection:
 	var node_a: UmlNode
 	var node_b: UmlNode
+	var type: ConnectionType
 	
-	func _init(node_a: UmlNode, node_b: UmlNode):
+	func _init(node_a: UmlNode, node_b: UmlNode, connection_type: ConnectionType):
 		self.node_a = node_a
 		self.node_b = node_b
+		self.type = connection_type
