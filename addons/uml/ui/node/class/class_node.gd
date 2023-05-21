211b095d2e05be65b0d1d256c87fd17918e1df80
igorecha.9999 <igorecha.9999@gmail.com>
2023-05-21 13:01:35 +0700

    Add class uml node members descriptions

--- addons/uml/ui/node/class/class_node.gd
+++ addons/uml/ui/node/class/class_node.gd
@@ -1,1 +1,38 @@
+class_name ClassNode
 extends UmlNode
+
+
+enum AccessModifier {
+	PUBLIC,
+	PRIVATE,
+}
+
+
+var _node_class_name: String = "Class"
+
+var _signals: Array[SignalDescription]
+var _variables: Array[VariableDescription]
+var _methods: Array[MethodDescription]
+
+
+class VariableDescription:
+	var access_modifier: AccessModifier
+	var name: String
+	var type: String
+
+
+class MethodArgumentDescription:
+	var name: String
+	var type: String
+
+
+class MethodDescription:
+	var access_modifier: AccessModifier
+	var name: String
+	var arguments: Array[MethodArgumentDescription]
+	var return_type: String
+
+
+class SignalDescription:
+	var name: String
+	var arguments: Array[MethodArgumentDescription]
