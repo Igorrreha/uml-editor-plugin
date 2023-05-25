45d7538e2821ee9e11c95409c72a65662d6bbcc7
igorecha.9999 <igorecha.9999@gmail.com>
2023-05-25 23:33:16 +0700

    Fix class node context menu popup position

--- addons/uml/ui/node/class/popup_menu.gd
+++ addons/uml/ui/node/class/popup_menu.gd
@@ -8,18 +8,18 @@ func _on_id_pressed(id: int) -> void:
 	match id:
 		0:
 			_class_node_members_container.add_variable()
-#		1:
-#			_class_node_members_container.add_method()
-#		2:
-#			_class_node_members_container.add_signal()
+		1:
+			_class_node_members_container.add_method()
+		2:
+			_class_node_members_container.add_signal()
 
 
 func _on_class_node_gui_input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton
 	and event.button_index == MOUSE_BUTTON_RIGHT
 	and event.is_pressed()):
-		open()
+		open(event.global_position)
 
 
-func open():
-	popup(Rect2(get_mouse_position(), size))
+func open(popup_position: Vector2) -> void:
+	popup(Rect2(popup_position, size))
