86453fe2e706064fbcfc611587e89d4a9c521d50
igorecha.9999 <igorecha.9999@gmail.com>
2023-05-27 20:55:19 +0700

    Create shortcut to drop class name to parent class name field

--- addons/uml/ui/node/class/class_node_title_editor.gd
+++ addons/uml/ui/node/class/class_node_title_editor.gd
@@ -18,13 +18,12 @@ var _is_parent_class_locked: bool
 func on_target_node_gui_input(event: InputEvent) -> void:
 	if (not event is InputEventMouseButton
 	or event.position.y > _target_node.HEADER_HEIGHT
-	or (event.button_index != MOUSE_BUTTON_LEFT or not event.double_click)
-	or _edits_container.get_child_count() == 0):
+	or (event.button_index != MOUSE_BUTTON_LEFT or not event.double_click)):
 		return
 	
 	var popup_position = _target_node.global_position
 	
-	var childs_count = _edits_container.get_child_count()
+	var childs_count = 2
 	var child_size = Vector2(_target_node.size.x, _target_node.HEADER_HEIGHT)
 	var popup_size = child_size * Vector2(1, childs_count)
 	
@@ -35,6 +34,11 @@ func on_target_node_gui_input(event: InputEvent) -> void:
 	
 	_editing_popup.popup(Rect2(popup_position, popup_size * _target_node.scale))
 	opened.emit()
+
+
+func _set_current_class_as_parent() -> void:
+	_parent_class_name_edit.text = _class_name_edit.text
+	_on_parent_class_name_edit_text_changed(_class_name_edit.text)
 
 
 func _on_editing_popup_hide() -> void:
@@ -51,6 +55,7 @@ func _on_parent_class_name_edit_text_changed(text: String) -> void:
 
 
 func _set_parent_class_lock(locked: bool) -> void:
+	_is_parent_class_locked = locked
 	_parent_class_name_edit.editable = not locked
 
 
