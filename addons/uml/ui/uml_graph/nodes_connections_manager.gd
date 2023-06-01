a8c03f30e22545d46dc60d1f5b8a0e6c176c8547
igorecha.9999 <igorecha.9999@gmail.com>
2023-06-01 23:10:55 +0700

    Remove duplicate method

--- addons/uml/ui/uml_graph/nodes_connections_manager.gd
+++ addons/uml/ui/uml_graph/nodes_connections_manager.gd
@@ -55,18 +55,6 @@ func _actualize_cross_connections() -> void:
 	_cross_connections.make_read_only()
 
 
-func get_cross_connections() -> Array[UmlConnection]:
-	var cross_connections: Array[UmlConnection]
-	for node_from in _connections:
-		for node_to in _connections[node_from]:
-			if (_connections.has(node_to)
-			and _connections[node_to].has(node_from)):
-				cross_connections.append(_connections[node_from][node_to])
-	
-	cross_connections.make_read_only()
-	return cross_connections
-
-
 func _connect(node_a: UmlNode, node_b: UmlNode, connection_type: UmlConnection.Type) -> void:
 	if not _connections.has(node_a):
 		_connections[node_a] = {}
