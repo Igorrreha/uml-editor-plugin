b294546559fac0c42c5da0f0c86d339cb7f85ce8
igorecha.9999 <igorecha.9999@gmail.com>
2023-06-02 21:04:04 +0700

    Fix graph node spawn position

--- addons/uml/ui/nodes_creator.gd
+++ addons/uml/ui/nodes_creator.gd
@@ -2,10 +2,11 @@ extends Node
 
 
 @export var _tscn_class_node: PackedScene
-@export var _container: Node
+@export var _graph: UmlGraph
 
 
 func create_class_node(position: Vector2) -> void:
 	var new_node = _tscn_class_node.instantiate() as GraphNode
-	new_node.position_offset = position
-	_container.add_child(new_node)
+	new_node.position_offset = ((position + _graph.scroll_offset) / _graph.zoom
+		- _graph.global_position)
+	_graph.add_child(new_node)
